2da24d5c92e762d4049ed4bbd90d86fabd8f6540
Jesse Tolj <jesse@objectfactory.io>
2025-08-14 09:54:51 -0400

    trying out svim. clean up sketchybar config

--- sketchybar/.config/sketchybar/items/init.lua
+++ sketchybar/.config/sketchybar/items/init.lua
@@ -1,8 +1,6 @@
 require("items.apple")
 
 require("items.aerospace")
--- require("items.menus")
--- require("items.spaces")
 require("items.front_app")
 require("items.calendar")
 require("items.widgets")
