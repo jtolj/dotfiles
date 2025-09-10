93008edd97089b17d38239cc3daf81c52ea52d55
Jesse Tolj <jesse@objectfactory.io>
2025-09-10 13:37:41 -0400

    add trim.lua

---
+++ neovim/dot-config/nvim/lua/plugins/trim.lua
@@ -0,0 +1,15 @@
+return {
+  'cappyzawa/trim.nvim',
+  opts = {
+    ft_blocklist = {},
+    patterns = {},
+    trim_on_write = true,
+    trim_trailing = true,
+    trim_last_line = true,
+    trim_first_line = true,
+    highlight = false,
+    highlight_bg = '#ff0000',
+    highlight_ctermbg = 'red',
+    notifications = true,
+  },
+}
