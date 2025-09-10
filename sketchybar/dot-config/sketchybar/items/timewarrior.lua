3de657e080080767c92a3db86257865a8e4b34e1
Jesse Tolj <jesse@objectfactory.io>
2025-09-10 15:58:46 -0400

    switch to events for sketchybar/timewarrior

--- sketchybar/dot-config/sketchybar/items/timewarrior.lua
+++ sketchybar/dot-config/sketchybar/items/timewarrior.lua
@@ -2,9 +2,9 @@ local colors = require("colors")
 local icons = require("icons")
 local settings = require("settings")
 
+sbar.add("event", "timewarrior_updated")
+
 local timewarrior = sbar.add("item", {
-	script = "$CONFIG_DIR/helpers/timew.sh",
-	update_freq = 5,
 	padding_left = 1,
 	padding_right = 1,
 	position = "right",
@@ -14,6 +14,7 @@ local timewarrior = sbar.add("item", {
 		border_width = 1,
 	},
 	label = {
+		drawing = false,
 		padding_right = 8,
 	},
 	icon = {
@@ -24,3 +25,28 @@ local timewarrior = sbar.add("item", {
 		color = colors.grey,
 	},
 })
+
+timewarrior:subscribe("timewarrior_updated", function(env)
+	if env.ACTIVE == "0" then
+		timewarrior:set({
+			label = { drawing = false },
+			icon = {
+				padding_left = 8,
+				padding_right = 10,
+				color = colors.grey,
+			},
+		})
+	else
+		timewarrior:set({
+			label = {
+				string = env.LABEL,
+				drawing = true,
+			},
+			icon = {
+				padding_left = 8,
+				padding_right = 10,
+				color = colors.yellow,
+			},
+		})
+	end
+end)
