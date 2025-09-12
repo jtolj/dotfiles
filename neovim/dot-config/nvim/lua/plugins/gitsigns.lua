83f80911dda11e6d55b4bda82ba69e546a85f0d2
Jesse Tolj <jesse@objectfactory.io>
2025-09-12 10:47:57 -0400

    add keyboard shortcuts for gitsigns, clean up zshrc

--- neovim/dot-config/nvim/lua/plugins/gitsigns.lua
+++ neovim/dot-config/nvim/lua/plugins/gitsigns.lua
@@ -2,6 +2,10 @@ return
 -- See `:help gitsigns` to understand what the configuration keys do
 { -- Adds git related signs to the gutter, as well as utilities for managing changes
   'lewis6991/gitsigns.nvim',
+  keys = {
+    { '<leader>ghb', '<cmd>Gitsigns toggle_current_line_blame<cr>', desc = 'Git toggle current line blame' },
+    { '<leader>ghB', '<cmd>Gitsigns blame<cr>', desc = 'Git blame' },
+  },
   opts = {
     signs = {
       add = { text = '+' },
