b1e79786789ee19e46fb9c58107c3332e3f996d5
Jesse Tolj <jesse@objectfactory.io>
2025-09-18 08:05:44 -0400

    add filename to lualine

--- neovim/dot-config/nvim/lua/plugins/lualine.lua
+++ neovim/dot-config/nvim/lua/plugins/lualine.lua
@@ -81,25 +81,8 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true },
   opts = {
     theme = 'auto',
-    inactive_sections = {
-      lualine_a = {
-        {
-          'filename',
-          symbols = {
-            modified = ' ●',
-            readonly = ' ',
-            unnamed = '',
-          },
-        },
-      },
-      lualine_b = {},
-      lualine_c = {},
-      lualine_x = {},
-      lualine_y = {},
-      lualine_z = {},
-    },
     sections = {
-      lualine_a = { mode },
+      lualine_a = { mode, 'filename' },
       lualine_b = { 'branch', 'diff', 'diagnostics' },
       lualine_c = {},
       lualine_x = { 'encoding', 'fileformat', 'filetype' },
