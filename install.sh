6835df9f344c1a35277aceb37e0d15e9a9afe2ee
Jesse Tolj <jesse@objectfactory.io>
2025-09-18 09:18:12 -0400

    simplify directory structure

--- install.sh
+++ install.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-stow --dotfiles -v -t ~ -d . */
+set -e
+
+# Files that live in $XDG_CONFIG_HOME
+xdg_config_files=("aerospace" "asciinema" "atuin" "gh-dash"	"infat"	"mise" "nvim"	"oh-my-posh" "sketchybar" "svim" "wezterm")
+
+for pkg in "${xdg_config_files[@]}"; do
+	echo "Stowing $pkg in $HOME/.config/$pkg"
+	mkdir -p "$HOME/.config/$pkg" && stow --restow -v -t "$HOME/.config/$pkg" "$pkg"
+done
+
+# Files that live in home directory
+home_dir_files=("hammerspoon" "git" "homebrew" "zsh")
+for pkg in "${home_dir_files[@]}"; do
+	echo "Stowing $pkg contents in home directory"
+	stow --dotfiles --restow -v -t ~ "$pkg"
+done
