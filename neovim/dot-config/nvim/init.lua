edbe98067e0cbd70d748a6fe14f76a670d031866
Jesse Tolj <jesse@objectfactory.io>
2025-09-18 08:04:52 -0400

    restore arrow key hard-time

--- neovim/dot-config/nvim/init.lua
+++ neovim/dot-config/nvim/init.lua
@@ -327,6 +327,12 @@ end
 
 vim.keymap.set('n', 'z!', toggle_spell, { desc = 'Toggle spell checking', silent = true })
 
+-- Disable arrow keys in normal mode
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
 -- [[ Configure and install plugins ]]
 --
 --  To check the current status of your plugins, run
