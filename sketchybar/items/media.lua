1e431e0c8c07ae34e7095ddc45a0025fc81bc2fc
Jesse Tolj <jesse@objectfactory.io>
2025-10-03 10:23:53 -0400

    Move sketchybar media.lua to use media-control for event driven updates.

--- sketchybar/items/media.lua
+++ sketchybar/items/media.lua
@@ -1,26 +1,25 @@
 local icons = require("icons")
+local app_icons = require("helpers/app_icons")
 local colors = require("colors")
 
 sbar.add("event", "media_change_custom")
--- sbar.exec(
--- 	"killall media_change >/dev/null; $CONFIG_DIR/helpers/event_providers/media_change/bin/media_change media_change_custom 2.0"
--- )
+sbar.exec("$CONFIG_DIR/helpers/event_providers/media_change_custom/media_change")
 
 local media_icon = sbar.add("item", "media_icon", {
+	click_script = "media-control toggle-play-pause",
 	updates = true,
 	drawing = false,
 	position = "right",
 	scroll_texts = false,
-	padding_left = 0,
-	padding_right = 0,
-	width = 16,
-	icon = {
-		padding_right = 16,
+	label = {
+		width = 18,
 		string = icons.media.play,
 		drawing = true,
 		color = colors.white,
 	},
-	label = {
+	icon = {
+		width = 18,
+		font = { family = "sketchybar-app-font", style = "Regular", size = 16 },
 		drawing = false,
 	},
 })
@@ -28,20 +27,16 @@ local media_icon = sbar.add("item", "media_icon", {
 local media_artist = sbar.add("item", "media_artist", {
 	updates = true,
 	drawing = false,
-	script = "$CONFIG_DIR/helpers/media-change.sh",
-	update_freq = 3,
 	y_offset = -6,
 	position = "right",
 	scroll_texts = false,
-	padding_left = 14,
-	padding_right = 30,
 	width = 0,
 	label = {
 		drawing = true,
 		font = {
 			size = 9,
 		},
-		max_chars = 32,
+		max_chars = 16,
 	},
 })
 
@@ -50,27 +45,24 @@ local media_title = sbar.add("item", "media_title", {
 	position = "right",
 	scroll_texts = false,
 	drawing = false,
-	padding_left = 14,
-	padding_right = 30,
 	updates = true,
 	width = 0,
-	icon = {
-		drawing = false,
-	},
 	label = {
 		drawing = true,
 		font = {
 			size = 9,
 		},
-		max_chars = 32,
+		max_chars = 16,
 	},
 })
+
 local media_bracket = sbar.add("bracket", "widgets.media.bracket", {
 	media_title.name,
 	media_artist.name,
 	media_icon.name,
 }, {
-	background = { color = colors.bg1 },
+	width = 128,
+	background = { color = colors.bg1, padding_left = 10, padding_right = 10 },
 })
 
 media_title:subscribe("mouse.entered", function()
@@ -94,18 +86,35 @@ end)
 media_title:subscribe("media_change_custom", function(env)
 	local drawing = (env.STATE ~= "gone")
 
-	if env.APP_NAME == "TIDAL" then
-	else
-	end
-
 	local icon = icons.media.play
+
+	if (env.APP_NAME == nil) or (env.APP_NAME == "") then
+		media_icon:set({
+			icon = {
+				string = "",
+				drawing = false,
+			},
+		})
+	else
+		sbar.exec("lsappinfo info -only bundlename " .. env.APP_NAME, function(result)
+			local app_name = result:match('"CFBundleName"="([^"]+)"')
+			local app_icon_str = app_icons[app_name] or ":music:"
+
+			media_icon:set({
+				icon = {
+					string = app_icon_str,
+					drawing = drawing,
+				},
+			})
+		end)
+	end
 
 	if env.STATE == "playing" then
 		icon = icons.media.pause
 	end
 
 	media_icon:set({
-		icon = {
+		label = {
 			string = icon,
 		},
 		drawing = drawing,
