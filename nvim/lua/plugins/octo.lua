9d51d7844f6913f7db67b13bfcf9cab8bdb201f0
Jesse Tolj <jesse@objectfactory.io>
2025-10-07 15:33:47 -0400

    add octo.nvim

---
+++ nvim/lua/plugins/octo.lua
@@ -0,0 +1,12 @@
+return {
+  'pwntester/octo.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'folke/snacks.nvim',
+    'nvim-tree/nvim-web-devicons',
+  },
+  opts = {
+    picker = 'snacks',
+    default_delete_branch = true,
+  },
+}
