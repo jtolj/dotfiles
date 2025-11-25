51e8fd50542d059a719a2220b232218279f5b104
Jesse Tolj <jesse@objectfactory.io>
2025-11-25 09:50:12 -0500

    nvim: conform - toggle autoformat

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,6 +4,21 @@ return { -- Autoformat
   cmd = { 'ConformInfo' },
   keys = {
     {
+      '<leader>c!',
+      function()
+        -- If autoformat is currently disabled for this buffer,
+        -- then enable it, otherwise disable it
+        if vim.b.disable_autoformat then
+          vim.cmd 'FormatEnable'
+          vim.notify 'Enabled autoformat for current buffer'
+        else
+          vim.cmd 'FormatDisable!'
+          vim.notify 'Disabled autoformat for current buffer'
+        end
+      end,
+      desc = 'Toggle autoformat for current buffer',
+    },
+    {
       '<leader>cf',
       function()
         require('conform').format { async = true, lsp_format = 'fallback' }
@@ -12,15 +27,41 @@ return { -- Autoformat
       desc = '[F]ormat buffer',
     },
   },
+  config = function(_, opts)
+    require('conform').setup(opts)
+
+    vim.api.nvim_create_user_command('FormatDisable', function(args)
+      if args.bang then
+        -- :FormatDisable! disables autoformat for this buffer only
+        vim.b.disable_autoformat = true
+      else
+        -- :FormatDisable disables autoformat globally
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = 'Disable autoformat-on-save',
+      bang = true, -- allows the ! variant
+    })
+
+    vim.api.nvim_create_user_command('FormatEnable', function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = 'Re-enable autoformat-on-save',
+    })
+  end,
   opts = {
     notify_on_error = true,
     format_on_save = function(bufnr)
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
       local disable_filetypes = { c = true, cpp = true }
       if disable_filetypes[vim.bo[bufnr].filetype] then
         return nil
       else
         return {
-          timeout_ms = 4000,
+          timeout_ms = 700,
           lsp_format = 'fallback',
         }
       end
