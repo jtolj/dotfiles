01e3e3f9a27ef2551e5b2d9a88c88fff83085a00
Jesse Tolj <jesse@objectfactory.io>
2025-12-05 09:56:33 -0500

    sketchybar: simplify/improve aerospace spaces and front_app

--- sketchybar/items/aerospace.lua
+++ sketchybar/items/aerospace.lua
@@ -6,6 +6,70 @@ local app_icons = require("helpers.app_icons")
 sbar.add("event", "aerospace_workspace_change")
 sbar.add("event", "aerospace_started")
 
+-- Sketchybar doesn't support asynchronously adding items to a bracket, so pre-rendering these to
+-- have control of the order of where this element is added to the bar.
+local aerospace_watcher = sbar.add("item", "aerospace_watcher", {
+	drawing = true,
+	label = {
+		drawing = false,
+	},
+	icon = {
+		drawing = false,
+	},
+})
+
+local spaces_table = {}
+for i = 1, 6 do
+	spaces_table[i] = sbar.add("item", "space." .. i, {
+		drawing = false,
+		label = {
+			font = {
+				family = settings.font.numbers,
+			},
+			highlight = false,
+			string = i,
+			padding_left = 1,
+			padding_right = 1,
+			color = colors.grey,
+			highlight_color = colors.white,
+		},
+		click_script = "aerospace workspace " .. i,
+	})
+end
+
+-- Subscribe to workspace changes from our dummy item so we only need one watcher
+local current_workspace = nil
+aerospace_watcher:subscribe("aerospace_workspace_change", function(env)
+	local space = env.FOCUSED_WORKSPACE and spaces_table[tonumber(env.FOCUSED_WORKSPACE)]
+	local prev_space = current_workspace and spaces_table[tonumber(current_workspace)]
+
+	if space then
+		current_workspace = env.FOCUSED_WORKSPACE
+		space:set({
+			icon = {
+				highlight = true,
+			},
+			label = {
+				highlight = true,
+			},
+			background = {
+				border_color = colors.white,
+			},
+		})
+	end
+
+	if prev_space then
+		prev_space:set({
+			label = {
+				highlight = false,
+			},
+			background = {
+				border_color = colors.transparent,
+			},
+		})
+	end
+end)
+
 sbar.exec(
 	"aerospace list-workspaces --all --json --format %{workspace}%{workspace-is-focused}%{monitor-id}%{monitor-name}",
 	function(workspaces)
@@ -16,54 +80,36 @@ sbar.exec(
 			local monitor_id = workspace["monitor-id"]
 			local monitor_name = workspace["monitor-name"]
 
-			local space = sbar.add("item", "space." .. sid, {
-				icon = {
-					font = {
-						family = settings.font.numbers,
-					},
-					highlight = isFocused,
-					string = sid,
-					padding_left = 15,
-					padding_right = 8,
-					color = colors.grey,
-					highlight_color = colors.white,
-				},
-				padding_right = 1,
-				padding_left = 1,
-				background = {
-					color = colors.with_alpha(colors.bg1, 0.8),
-					border_width = 1,
-					height = 26,
-					border_color = isFocused and colors.white or colors.bg2,
-					padding_right = 3,
-					padding_left = 3,
-				},
-				click_script = "aerospace workspace " .. sid,
+			local space = spaces_table[tonumber(sid)]
+
+			space:set({
+				drawing = true,
 			})
 
-			if last_monitor ~= monitor_id then
+			if last_monitor ~= monitor_id and monitor_id > 1 then
 				space:set({
-					padding_left = 10,
+					icon = {
+						string = "|",
+						padding_right = 16,
+						color = colors.grey,
+						highlight_color = colors.white,
+					},
+					padding_left = 8,
 				})
 				last_monitor = monitor_id
 			end
 
-			-- Subscribe to workspace changes
-			space:subscribe("aerospace_workspace_change", function(env)
-				local selected = (sid == env.FOCUSED_WORKSPACE)
-
+			if isFocused then
+				current_workspace = sid
 				space:set({
-					icon = {
-						highlight = selected,
-					},
 					label = {
-						highlight = selected,
+						highlight = true,
 					},
 					background = {
-						border_color = selected and colors.white or colors.bg2,
+						border_color = colors.white,
 					},
 				})
-			end)
+			end
 		end
 	end
 )
--- sketchybar/items/front_app.lua
+++ sketchybar/items/front_app.lua
@@ -2,7 +2,6 @@ local colors = require("colors")
 local settings = require("settings")
 
 local front_app = sbar.add("item", "front_app", {
-	display = "active",
 	icon = { drawing = false },
 	label = {
 		font = {
@@ -15,8 +14,4 @@ local front_app = sbar.add("item", "front_app", {
 
 front_app:subscribe("front_app_switched", function(env)
 	front_app:set({ label = { string = env.INFO } })
-end)
-
-front_app:subscribe("mouse.clicked", function(env)
-	sbar.trigger("swap_menus_and_spaces")
 end)
