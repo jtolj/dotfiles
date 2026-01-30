88102e1331dda217032bd1af3d90a09441dc62e6
Jesse Tolj <jesse@objectfactory.io>
2026-01-30 16:42:42 -0500

    sketchybar: reference submodule version of sketchybar-system-stats

--- sketchybar/items/widgets/init.lua
+++ sketchybar/items/widgets/init.lua
@@ -1,6 +1,7 @@
 sbar.exec(
-	"killall system_stats >/dev/null; $HOME/.local/bin/stats_provider -m ram_usage memory_pressure -c usage -n en0 --no-units"
+	"killall system_stats >/dev/null; $CONFIG_DIR/helpers/event_providers/sketchybar-system-stats/target/release/stats_provider -m ram_usage -c usage -n en0 --no-units"
 )
+
 require("items.widgets.battery")
 require("items.widgets.wifi")
 require("items.widgets.cpu")
