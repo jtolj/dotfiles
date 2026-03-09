090587f89eeaabdafcb3d0e10b4ceccf15bb4b6c
Jesse Tolj <jesse@objectfactory.io>
2026-03-09 08:32:18 -0400

    neovim: render-markdown remove comment

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -2,8 +2,8 @@
 ---@type LazySpec
 return {
   'MeanderingProgrammer/render-markdown.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-  -- -@module 'render-markdown'
-  -- -@type render.md.UserConfig
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
   opts = {},
 }
