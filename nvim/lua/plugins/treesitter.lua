3a8c36b1f56e8f795a42aeffa6243f5da1e7159a
Jesse Tolj <jesse@objectfactory.io>
2026-04-08 13:00:35 -0400

    neovim: remove notify on grammar not installed

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,113 +1,113 @@
 ---@module "lazy"
 ---@type LazySpec
+
 return {
-  'nvim-treesitter/nvim-treesitter',
-  dependencies = {
-    {
-      'nvim-treesitter/nvim-treesitter-context',
-      opts = {
-        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-        multiwindow = false, -- Enable multiwindow support.
-        max_lines = 256, -- How many lines the window should span. Values <= 0 mean no limit.
-        min_window_height = 60, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-        line_numbers = true,
-        multiline_threshold = 20, -- Maximum number of lines to show for a single context
-        trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-        mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
-        -- Separator between context and content. Should be a single character string, like '-'.
-        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-        separator = nil,
-        zindex = 20, -- The Z-index of the context window
-        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-      },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    opts = {
+      enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+      multiwindow = false, -- Enable multiwindow support.
+      max_lines = 256, -- How many lines the window should span. Values <= 0 mean no limit.
+      min_window_height = 60, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+      line_numbers = true,
+      multiline_threshold = 20, -- Maximum number of lines to show for a single context
+      trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+      mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+      -- Separator between context and content. Should be a single character string, like '-'.
+      -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+      separator = nil,
+      zindex = 20, -- The Z-index of the context window
+      on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
     },
   },
-  lazy = false,
-  branch = 'main',
-  build = ':TSUpdate',
-  config = function()
-    local ts = require 'nvim-treesitter'
+  {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    branch = 'main',
+    build = ':TSUpdate',
+    config = function()
+      local ts = require 'nvim-treesitter'
 
-    vim.api.nvim_create_autocmd('User', {
-      pattern = 'TSUpdate',
-      callback = function()
-        require('nvim-treesitter.parsers').svelte = {
-          install_info = {
-            url = 'https://github.com/jtolj/tree-sitter-svelte',
-            branch = 'v1.0.3',
-          },
-        }
-      end,
-    })
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'TSUpdate',
+        callback = function()
+          require('nvim-treesitter.parsers').svelte = {
+            install_info = {
+              url = 'https://github.com/jtolj/tree-sitter-svelte',
+              branch = 'v1.0.3',
+            },
+          }
+        end,
+      })
 
-    -- Install core parsers at startup
-    ts.install {
-      'bash',
-      'blade',
-      'caddy',
-      'comment',
-      'clojure',
-      'cpp',
-      'css',
-      'diff',
-      'git_config',
-      'git_rebase',
-      'gitcommit',
-      'gitignore',
-      'go',
-      'html',
-      'java',
-      'javascript',
-      'json',
-      'kotlin',
-      'lua',
-      'luadoc',
-      'make',
-      'markdown',
-      'markdown_inline',
-      'python',
-      'php',
-      'ruby',
-      'query',
-      'regex',
-      'rust',
-      'scss',
-      'swift',
-      'svelte',
-      'toml',
-      'tsx',
-      'typescript',
-      'typst',
-      'vim',
-      'vimdoc',
-      'vue',
-      'yaml',
-      'xml',
-    }
+      -- Install core parsers at startup
+      ts.install {
+        'bash',
+        'blade',
+        'caddy',
+        'comment',
+        'clojure',
+        'cpp',
+        'css',
+        'diff',
+        'git_config',
+        'git_rebase',
+        'gitcommit',
+        'gitignore',
+        'go',
+        'html',
+        'java',
+        'javascript',
+        'json',
+        'kotlin',
+        'lua',
+        'luadoc',
+        'make',
+        'markdown',
+        'markdown_inline',
+        'python',
+        'php',
+        'ruby',
+        'query',
+        'regex',
+        'rust',
+        'scss',
+        'swift',
+        'svelte',
+        'toml',
+        'tsx',
+        'typescript',
+        'typst',
+        'vim',
+        'vimdoc',
+        'vue',
+        'yaml',
+        'xml',
+      }
 
-    local group = vim.api.nvim_create_augroup('TreesitterSetup', { clear = true })
-    -- Enable highlighting on FileType
-    vim.api.nvim_create_autocmd('FileType', {
-      group = group,
-      desc = 'Enable treesitter highlighting and indentation',
-      callback = function(event)
-        local lang = vim.treesitter.language.get_lang(event.match)
-        if not lang then
-          vim.notify('No treesitter parser for ' .. event.match .. ' found')
-          return
-        end
+      local group = vim.api.nvim_create_augroup('TreesitterSetup', { clear = true })
+      -- Enable highlighting on FileType
+      vim.api.nvim_create_autocmd('FileType', {
+        group = group,
+        desc = 'Enable treesitter highlighting and indentation',
+        callback = function(event)
+          local lang = vim.treesitter.language.get_lang(event.match)
+          if not lang then
+            return
+          end
 
-        local buf = event.buf
-        -- Start highlighting immediately (works if parser exists)
-        pcall(vim.treesitter.start, buf, lang)
+          local buf = event.buf
+          -- Start highlighting immediately (works if parser exists)
+          pcall(vim.treesitter.start, buf, lang)
 
-        -- Enable folding
-        vim.wo[0][0].foldmethod = 'expr'
-        vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          -- Enable folding
+          vim.wo[0][0].foldmethod = 'expr'
+          vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 
-        -- Enable indentation
-        vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      end,
-    })
-  end,
+          -- Enable indentation
+          vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
+  },
 }
