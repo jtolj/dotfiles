37c204c274ad9776ac0ec67eb93f7ffc2d231d85
Jesse Tolj <jesse@objectfactory.io>
2026-05-18 08:39:41 -0400

    nvim: move mise functionality to plugin and add fzf integration

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -1,3 +1,1 @@
-vim.api.nvim_create_user_command('Mise', function(args)
-  vim.cmd('!mise run ' .. args.args)
-end, { desc = 'Execute mise run', nargs = '*' })
+
---
+++ nvim/plugin/mise.lua
@@ -0,0 +1,48 @@
+vim.api.nvim_create_user_command('Mise', function(args)
+  vim.cmd('term mise run ' .. args.args)
+end, { desc = 'Execute mise run', nargs = '*' })
+
+vim.api.nvim_create_user_command('MisePick', function(args)
+  local fzf = require 'fzf-lua'
+  local command = { 'mise', 'tasks', '--json' }
+
+  local result = vim.system(command, { text = true }):wait()
+  if result.code ~= 0 then
+    return vim.notify(vim.inspect(result))
+  end
+
+  local items_json = vim.json.decode(result.stdout)
+
+  local tasks = {}
+  local task_data = {}
+  for _, task in ipairs(items_json) do
+    local name = string.format('%-30s %s', task.name, task.description)
+    table.insert(tasks, name)
+    task_data[name] = task
+  end
+
+  if #tasks == 0 then
+    vim.notify 'No mise tasks were found.'
+    return
+  end
+
+  require('fzf-lua').fzf_exec(tasks, {
+    prompt = 'mise task > ',
+    actions = {
+      ['default'] = function(selected, opts)
+        if selected[1] == nil then
+          return
+        end
+        local task_details = task_data[selected[1]]
+        local task = string.sub(selected[1], 1, 30)
+        -- vim.notify(vim.inspect(task_details))
+        -- TODO: handle providing argument(s) with vim.ui.input
+        vim.cmd('term mise run ' .. task:match '(.+)%s*$')
+      end,
+    },
+  })
+end, { desc = 'Mise task picker' })
+
+vim.keymap.set('n', '<leader>M', function()
+  vim.cmd 'MisePick'
+end, { desc = 'Pick mise task' })
