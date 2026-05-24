5b728949bfb13e402bb8ffe29d4fd3d4302c8a3f
Jesse Tolj <jesse@objectfactory.io>
2026-05-24 09:19:01 -0400

    nvim: replace todo-comments with mini-hipatterns

---
+++ nvim/plugin/mini-hipatterns.lua
@@ -0,0 +1,11 @@
+vim.pack.add { 'https://github.com/nvim-mini/mini.nvim' }
+local hipatterns = require 'mini.hipatterns'
+hipatterns.setup {
+  highlighters = {
+    -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+    fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+    hack = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
+    todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
+    note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
+  },
+}
